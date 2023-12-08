84aae66afa709ddaa0724d901e1f2b8e5d1de6dc
Christian Hein <christian@christianhein.com>
2023-12-08 12:16:56 +0100

    Add generic Pair class

---
+++ src/main/java/com/christianhein/aoc2023/util/Pair.java
@@ -0,0 +1,23 @@
+package com.christianhein.aoc2023.util;
+
+import java.util.Objects;
+
+public record Pair<T, E>(T left, E right) {
+    @Override
+    public String toString() {
+        return "(" + left + ", " + right + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) && Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}
